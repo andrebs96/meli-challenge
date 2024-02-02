646209d47527993e5bfb39ecf0c021b30fe2f75a
André Beserra de Souza <andre.souza@cobasi.com.br>
2024-02-01 23:47:05 -0300

    refactor(home): Importando componentes

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,36 +1,16 @@
 import React from 'react'
-import { Search } from '@styled-icons/evil/Search'
 
 import * as S from './styles'
 
-import Logo from 'assets/img/pt_logo_large_plus.webp'
+import Logo from 'components/Logo'
+import Search from 'components/Search'
 
-const Home = () => (
+const Home = ({ onSubmit }) => (
   <S.Wrapper>
     <S.Content>
       <S.Heading>Mercado Livre</S.Heading>
-      <S.Logo
-        src={Logo}
-        alt="Mercado Livre Brasil - Onde comprar e vender de Tudo"
-      />
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          console.log('oi')
-        }}
-      >
-        <S.InputField>
-          <input
-            type="text"
-            placeholder="Buscar produtos, marcas e muito mais…"
-            maxLength="120"
-            autoComplete="off"
-          />
-          <button type="submit">
-            <Search size="30" />
-          </button>
-        </S.InputField>
-      </form>
+      <Logo marginBottom="2.4rem" />
+      <Search onSubmit={onSubmit} />
     </S.Content>
   </S.Wrapper>
 )
--- src/templates/Home/styles.js
+++ src/templates/Home/styles.js
@@ -17,34 +17,3 @@ export const Heading = styled.h1`
   z-index: -1;
   font-size: 0;
 `
-
-export const Logo = styled.img`
-  margin-bottom: 2.4rem;
-  width: 13rem;
-`
-
-export const InputField = styled.div`
-  align-items: center;
-  background-color: #ffffff;
-  box-shadow: 0 1px 2px 0 rgba(0, 0, 0, 0.2);
-  display: flex;
-  position: relative;
-  border-radius: 0.2rem;
-  padding: 1rem 1.6rem;
-  height: 4rem;
-  width: auto;
-
-  input {
-    border: 0;
-    outline: 0;
-    width: 35vw;
-    color: #000000e5;
-  }
-
-  button {
-    background-color: transparent;
-    border: 0;
-    color: #666;
-    outline: 0;
-  }
-`
