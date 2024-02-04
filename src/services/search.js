900484b416b9be56a0da00aba6f3bb707a7b519f
André Beserra de Souza <andre.souza@cobasi.com.br>
2024-02-03 23:33:32 -0300

    fix(pesquisa): adicionando o parâmetro status

--- src/services/search.js
+++ src/services/search.js
@@ -1,6 +1,6 @@
 import Api from './config'
 
 export const getSearchProducts = async (q) => {
-  const { data } = await Api.get(`/sites/MLB/search?q=${q}`)
+  const { data } = await Api.get(`/sites/MLB/search?status=active&q=${q}`)
   return data
 }
